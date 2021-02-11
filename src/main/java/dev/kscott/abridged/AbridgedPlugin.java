18e6b42ebf3873a84c402371a35a0f3ab5a2aea4
kadenscott <kscottdev@gmail.com>
2021-02-11 17:46:46 -0500

    rename base class

--- src/main/java/dev/kscott/abridged/AbridgedPlugin.java
+++ src/main/java/dev/kscott/abridged/AbridgedPlugin.java
@@ -2,7 +2,7 @@ package dev.kscott.abridged;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Abridged extends JavaPlugin {
+public final class AbridgedPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
