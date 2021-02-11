2ad9132de3084fce7d3a45d340be3301f794117c
kadenscott <kscottdev@gmail.com>
2021-02-11 17:45:35 -0500

    add project

---
+++ src/main/java/dev/kscott/abridged/Abridged.java
@@ -0,0 +1,17 @@
+package dev.kscott.abridged;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Abridged extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
