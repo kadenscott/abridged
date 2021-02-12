ef24aae1bef98d7106adb1e8f3b44afe44ba18b4
kadenscott <kscottdev@gmail.com>
2021-02-11 19:25:20 -0500

    register PlayerListeners

--- src/main/java/dev/kscott/abridged/AbridgedPlugin.java
+++ src/main/java/dev/kscott/abridged/AbridgedPlugin.java
@@ -5,6 +5,8 @@ import com.google.inject.Injector;
 import dev.kscott.abridged.inject.PluginModule;
 import dev.kscott.abridged.listeners.BlockListeners;
 import dev.kscott.abridged.listeners.OskarListener;
+import dev.kscott.abridged.listeners.PingListener;
+import dev.kscott.abridged.listeners.PlayerListeners;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
 
@@ -24,6 +26,8 @@ public final class AbridgedPlugin extends JavaPlugin {
 
         this.getServer().getPluginManager().registerEvents(injector.getInstance(BlockListeners.class), this);
         this.getServer().getPluginManager().registerEvents(injector.getInstance(OskarListener.class), this);
+        this.getServer().getPluginManager().registerEvents(injector.getInstance(PingListener.class), this);
+        this.getServer().getPluginManager().registerEvents(injector.getInstance(PlayerListeners.class), this);
     }
 
     @Override
