5b1187afd2c656fe214568f20c3a0973a4d321d2
kadenscott <kscottdev@gmail.com>
2021-02-11 19:37:37 -0500

    listen on BlockDispenseEvent

--- src/main/java/dev/kscott/abridged/listeners/BlockListeners.java
+++ src/main/java/dev/kscott/abridged/listeners/BlockListeners.java
@@ -1,14 +1,20 @@
 package dev.kscott.abridged.listeners;
 
 import com.google.inject.Inject;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockDispenseEvent;
+import org.bukkit.event.block.BlockDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.checkerframework.checker.nullness.qual.NonNull;
+import org.checkerframework.checker.nullness.qual.Nullable;
 
 import java.util.Random;
 
@@ -16,6 +22,15 @@ import java.util.Random;
  * Listens on {@link org.bukkit.event.block.BlockEvent}s.
  */
 public class BlockListeners implements Listener {
+
+    /**
+     * An array of {@link Material}s that can be dispensed by a dropper.
+     */
+    private static final Material[] dropperItemTypes = new Material[]{
+            Material.BLAZE_ROD,
+            Material.ENDER_EYE,
+            Material.PORKCHOP
+    };
 
     /**
      * The base block regeneration time, in ticks.
@@ -64,6 +79,44 @@ public class BlockListeners implements Listener {
                 state.update(true);
             }
         }.runTaskLater(plugin, BASE_REGEN_TIME + regenTimeModifier);
+    }
+
+    /**
+     * Cancels all dropper interactions.
+     *
+     * @param event {@link PlayerInteractEvent}.
+     */
+    @EventHandler
+    public void onDropperOpen(final @NonNull PlayerInteractEvent event) {
+        final @Nullable Block block = event.getClickedBlock();
+
+        if (block == null) {
+            return;
+        }
+
+        if (block.getType() != Material.DROPPER) {
+            return;
+        }
+
+        event.setCancelled(true);
+    }
+
+    /**
+     * Drops a predefined item when droppers are activated.
+     *
+     * @param event {@link BlockDispenseEvent}.
+     */
+    @EventHandler
+    public void onDropperDrop(final @NonNull BlockDispenseEvent event) {
+        final int choice = random.nextInt(dropperItemTypes.length);
+        final @NonNull Material material = dropperItemTypes[choice-1];
+
+        final int amount = random.nextInt(2)+1;
+
+        final @NonNull ItemStack stack = new ItemStack(material);
+        stack.setAmount(amount);
+
+        event.setItem(stack);
     }
 
 }
