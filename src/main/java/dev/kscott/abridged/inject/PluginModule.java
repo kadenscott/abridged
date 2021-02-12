a78de66d3c28bb6f62c4cc2259116a37afca8358
kadenscott <kscottdev@gmail.com>
2021-02-11 19:45:37 -0500

    bind to BukkitAudiences

--- src/main/java/dev/kscott/abridged/inject/PluginModule.java
+++ src/main/java/dev/kscott/abridged/inject/PluginModule.java
@@ -2,6 +2,7 @@ package dev.kscott.abridged.inject;
 
 import com.google.inject.AbstractModule;
 import dev.kscott.abridged.AbridgedPlugin;
+import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
@@ -17,12 +18,18 @@ public class PluginModule extends AbstractModule {
     private final @NonNull AbridgedPlugin plugin;
 
     /**
+     * {@link BukkitAudiences} instance.
+     */
+    private final @NonNull BukkitAudiences audiences;
+
+    /**
      * Constructs {@link PluginModule}.
      *
      * @param plugin {@link AbridgedPlugin} reference.
      */
     public PluginModule(final @NonNull AbridgedPlugin plugin) {
         this.plugin = plugin;
+        this.audiences = BukkitAudiences.create(plugin);
     }
 
     /**
@@ -32,5 +39,6 @@ public class PluginModule extends AbstractModule {
         this.bind(Plugin.class).toInstance(this.plugin);
         this.bind(JavaPlugin.class).toInstance(this.plugin);
         this.bind(AbridgedPlugin.class).toInstance(this.plugin);
+        this.bind(BukkitAudiences.class).toInstance(this.audiences);
     }
 }
